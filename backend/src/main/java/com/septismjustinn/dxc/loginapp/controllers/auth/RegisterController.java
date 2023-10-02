c292ad8673e05074ef213f67487a17c23d347a1c
Tan Justinn <ohhoehoe@gmail.com>
2023-10-02 19:14:38 +0800

    Implemented register endpoint

--- backend/src/main/java/com/septismjustinn/dxc/loginapp/controllers/auth/RegisterController.java
+++ backend/src/main/java/com/septismjustinn/dxc/loginapp/controllers/auth/RegisterController.java
@@ -1,9 +1,33 @@
 package com.septismjustinn.dxc.loginapp.controllers.auth;
 
+import com.septismjustinn.dxc.loginapp.models.User;
+import com.septismjustinn.dxc.loginapp.services.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/public/register")
 public class RegisterController {
+    private final UserService userService;
+
+    public RegisterController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public ResponseEntity<User> addNewUser(@RequestBody Map<String, String> req) {
+        System.out.println("Adding user");
+        try {
+            User newUser = userService.createUser(req.get("name"), req.get("username"), req.get("password"));
+            return new ResponseEntity(newUser, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
 }
