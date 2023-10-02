1672ce632dd843e5b85212a7c72fe26778a0ff83
Tan Justinn <ohhoehoe@gmail.com>
2023-10-02 14:47:46 +0800

    Prepared controllers for authentication

---
+++ backend/src/main/java/com/septismjustinn/dxc/loginapp/controllers/auth/LogoutController.java
@@ -0,0 +1,9 @@
+package com.septismjustinn.dxc.loginapp.controllers.auth;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/protected/logout")
+public class LogoutController {
+}
---
+++ backend/src/main/java/com/septismjustinn/dxc/loginapp/controllers/auth/RegisterController.java
@@ -0,0 +1,9 @@
+package com.septismjustinn.dxc.loginapp.controllers.auth;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/public/register")
+public class RegisterController {
+}
