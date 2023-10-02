f7fae80e2fe2e691ca1d10923e1fee931c86b928
Tan Justinn <ohhoehoe@gmail.com>
2023-10-02 16:04:57 +0800

    Added default password encrypter

--- backend/src/main/java/com/septismjustinn/dxc/loginapp/configs/SecurityConfig.java
+++ backend/src/main/java/com/septismjustinn/dxc/loginapp/configs/SecurityConfig.java
@@ -1,8 +1,12 @@
 package com.septismjustinn.dxc.loginapp.configs;
 
+import com.septismjustinn.dxc.loginapp.models.Role;
+import com.septismjustinn.dxc.loginapp.services.AppUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,9 +17,14 @@ public class SecurityConfig {
         return http.authorizeHttpRequests(
                 (authz) -> authz
                         .requestMatchers("/public/**").permitAll()
-                        .requestMatchers("/protected/admin").hasRole("Admin")
+                        .requestMatchers("/protected/admin").hasRole(Role.ADMIN.toString())
                         .requestMatchers("/protected/**").authenticated()
                 )
                 .build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
