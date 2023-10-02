51eaccb6890b821169c1f5d9af83ab5c1e035564
Tan Justinn <ohhoehoe@gmail.com>
2023-10-02 15:06:21 +0800

    Converted Role to Enum
    -No complicated manipulations needed for now other than to just check roles.
    -Removed associated table and relegated to enum

--- backend/src/main/java/com/septismjustinn/dxc/loginapp/models/Role.java
+++ backend/src/main/java/com/septismjustinn/dxc/loginapp/models/Role.java
@@ -1,28 +1,16 @@
 package com.septismjustinn.dxc.loginapp.models;
 
-import jakarta.persistence.*;
+public enum Role {
+    USER, ADMIN;
 
-import java.util.List;
-
-@Entity
-@Table(name = "roles")
-public class Role {
-    @Id
-    @Column(name = "ROLE_NAME")
-    private String rolename;
-
-    @OneToMany(mappedBy = "role", fetch = FetchType.LAZY)
-    private List<User> usersList;
-
-    public Role() {
-    }
-
-    public Role(String rolename, List<User> usersList) {
-        this.rolename = rolename;
-        this.usersList = usersList;
-    }
-
-    public String getRolename() {
-        return rolename;
+    @Override
+    public String toString() {
+        switch(this) {
+            case USER:
+                return "User";
+            case ADMIN:
+                return "Admin";
+        };
+        return "";
     }
 }
