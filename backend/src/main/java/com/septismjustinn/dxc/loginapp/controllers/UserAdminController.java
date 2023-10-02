029fea5ed06cc391618545f63da182076922a2ad
Tan Justinn <ohhoehoe@gmail.com>
2023-10-02 16:05:48 +0800

    Endpoint for admins to get all users
    -Temporarily unprotecting endpoint for testing

--- backend/src/main/java/com/septismjustinn/dxc/loginapp/controllers/UserAdminController.java
+++ backend/src/main/java/com/septismjustinn/dxc/loginapp/controllers/UserAdminController.java
@@ -1,9 +1,28 @@
 package com.septismjustinn.dxc.loginapp.controllers;
 
+import com.septismjustinn.dxc.loginapp.models.User;
+import com.septismjustinn.dxc.loginapp.services.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/protected/admin")
+@RequestMapping("/public/admin")
 public class UserAdminController {
+    private final UserService userService;
+
+    public UserAdminController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        System.out.println("Getting all users");
+        return new ResponseEntity<List<User>>(userService.getAllUsers(), HttpStatus.OK);
+    }
 }
