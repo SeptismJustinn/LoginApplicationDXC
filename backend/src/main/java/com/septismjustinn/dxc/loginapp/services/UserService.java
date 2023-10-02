be76f459d8ecb92c0b574959521ae03ac05c7fa5
Tan Justinn <ohhoehoe@gmail.com>
2023-10-02 19:11:49 +0800

    Added createUser function
    -Added function to validate and create new, unique users

--- backend/src/main/java/com/septismjustinn/dxc/loginapp/services/UserService.java
+++ backend/src/main/java/com/septismjustinn/dxc/loginapp/services/UserService.java
@@ -3,6 +3,8 @@ package com.septismjustinn.dxc.loginapp.services;
 import com.septismjustinn.dxc.loginapp.data.UserRepository;
 import com.septismjustinn.dxc.loginapp.models.Role;
 import com.septismjustinn.dxc.loginapp.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -12,6 +14,8 @@ import java.util.Optional;
 @Service
 public class UserService {
     private final UserRepository userRepo;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     public UserService(UserRepository userRepo) {
         this.userRepo = userRepo;
@@ -26,9 +30,15 @@ public class UserService {
         return new ArrayList<User>(userRepo.findAll());
     }
 
-    public User createUser(String name, String username, String password) {
-        String hash = "";
-        User newUser = new User(name, username, hash, Role.USER);
-        return newUser;
+    // Only to be run after verification
+    public User createUser(String name, String username, String password) throws Exception {
+        Optional<User> userCheck = getUser(username);
+        if (userCheck.isPresent()) {
+            throw new Exception("User already exists");
+        } else {
+            User newUser = new User(name, username, passwordEncoder.encode(password), Role.USER);
+            userRepo.save(newUser);
+            return newUser;
+        }
     }
 }
