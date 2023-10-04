89bafbfe4c722ee59f29f063472edd9a14c4c1e3
Tan Justinn <ohhoehoe@gmail.com>
2023-10-04 15:57:29 +0800

    Refactored LoginService to only handle manipulating repos during auth

--- backend/src/main/java/com/septismjustinn/dxc/loginapp/services/LoginService.java
+++ backend/src/main/java/com/septismjustinn/dxc/loginapp/services/LoginService.java
@@ -5,37 +5,40 @@ import com.septismjustinn.dxc.loginapp.data.UserRepository;
 import com.septismjustinn.dxc.loginapp.models.Login;
 import com.septismjustinn.dxc.loginapp.models.User;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.UUID;
 
 @Service
 public class LoginService {
     private final UserRepository userRepo;
     private final LoginRepository loginRepo;
-    private final JWTService jwtService;
     private final AuthenticationManager authenticationManager;
 
-    public LoginService(UserRepository userRepo, LoginRepository loginRepo, JWTService jwtService, AuthenticationManager authenticationManager) {
+    public LoginService(UserRepository userRepo, LoginRepository loginRepo, AuthenticationManager authenticationManager) {
         this.userRepo = userRepo;
         this.loginRepo = loginRepo;
-        this.jwtService = jwtService;
         this.authenticationManager = authenticationManager;
     }
 
-    public String login(String username, String password) {
+    public User login(String username, String password) throws BadCredentialsException {
         authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
         User user = userRepo.findByUsername(username)
-                .orElseThrow(()->new IllegalArgumentException("Invalid username/password"));
-        Map<String, Object> extraClaims = new HashMap<>();
-        extraClaims.put("name", user.getName());
-        extraClaims.put("role", user.getRole());
-        UUID jti = UUID.randomUUID();
-        extraClaims.put("jti", jti);
-        loginRepo.save(new Login(jti, false, null, user));
-        return jwtService.generateAccessToken(extraClaims, user);
+                .orElseThrow(()->new BadCredentialsException("Invalid username/password"));
+        return user;
+    }
+
+    public boolean registerLogin(UUID jti, User user) {
+        try {
+            // Remove all previous user logins
+            loginRepo.deleteByUser(user);
+            loginRepo.save(new Login(jti, user));
+            return true;
+        } catch (Exception e) {
+            // In case of errors in database
+            return false;
+        }
     }
 }
