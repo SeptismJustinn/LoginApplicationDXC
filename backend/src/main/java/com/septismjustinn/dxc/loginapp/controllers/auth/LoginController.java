cf5bb171279f17884cb25e8030f3b1c8af9f70ba
Tan Justinn <ohhoehoe@gmail.com>
2023-10-04 15:56:43 +0800

    Added login functionality
    Takes in username and password in body to verify against database

--- backend/src/main/java/com/septismjustinn/dxc/loginapp/controllers/auth/LoginController.java
+++ backend/src/main/java/com/septismjustinn/dxc/loginapp/controllers/auth/LoginController.java
@@ -1,9 +1,49 @@
 package com.septismjustinn.dxc.loginapp.controllers.auth;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.septismjustinn.dxc.loginapp.models.User;
+import com.septismjustinn.dxc.loginapp.services.JWTService;
+import com.septismjustinn.dxc.loginapp.services.LoginService;
+import com.septismjustinn.dxc.loginapp.validators.AuthRequest;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/public/login")
 public class LoginController {
+    LoginService loginService;
+    JWTService jwtService;
+
+    public LoginController(LoginService loginService, JWTService jwtService) {
+        this.loginService = loginService;
+        this.jwtService = jwtService;
+    }
+
+    @PostMapping
+    public ResponseEntity<String> loginUser(@RequestBody @Valid AuthRequest req) {
+        System.out.println("Logging in");
+        try {
+            System.out.println("Finding user");
+            User user = loginService.login(req.getUsername(), req.getPassword());
+            UUID jti = UUID.randomUUID();
+            System.out.println("Registering login");
+            boolean registered = loginService.registerLogin(jti, user);
+            if (true) {
+                System.out.println(user.getName() + " logged in");
+                return new ResponseEntity(jwtService.generateAccessToken(jti, user), HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>("Error with database", HttpStatus.BAD_GATEWAY);
+            }
+//            return new ResponseEntity<>(jwt, HttpStatus.OK);
+        } catch (BadCredentialsException e) {
+            return new ResponseEntity<>("Wrong username/password", HttpStatus.UNAUTHORIZED);
+        }
+    }
+
 }
