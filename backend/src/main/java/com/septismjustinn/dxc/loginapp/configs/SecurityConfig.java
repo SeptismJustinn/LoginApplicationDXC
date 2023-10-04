b43083f307ce953f95f06990191d69e05b4845a3
Tan Justinn <ohhoehoe@gmail.com>
2023-10-04 15:54:33 +0800

    Changed UserDetailsService to a Bean
    Did not work properly as a service as per my implementation

--- backend/src/main/java/com/septismjustinn/dxc/loginapp/configs/SecurityConfig.java
+++ backend/src/main/java/com/septismjustinn/dxc/loginapp/configs/SecurityConfig.java
@@ -1,27 +1,31 @@
 package com.septismjustinn.dxc.loginapp.configs;
 
+import com.septismjustinn.dxc.loginapp.data.UserRepository;
 import com.septismjustinn.dxc.loginapp.models.Role;
-import com.septismjustinn.dxc.loginapp.services.AppUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
-    AppUserDetailsService userDetailsService;
+    @Autowired
+    private UserRepository userRepo;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http.authorizeHttpRequests(
                 (authz) -> authz
                         .requestMatchers(HttpMethod.POST, "/public/register").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/public/login").permitAll()
                         .requestMatchers("/public/**").permitAll()
                         .requestMatchers("/protected/admin").hasRole(Role.ADMIN.toString())
                         .requestMatchers("/protected/**").authenticated()
@@ -37,9 +41,14 @@ public class SecurityConfig {
     }
 
     @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> userRepo.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+    @Bean
     public AuthenticationManager authenticationManager() {
         DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setUserDetailsService(userDetailsService());
         authProvider.setPasswordEncoder(getPasswordEncoder());
         return new ProviderManager(authProvider);
     }
