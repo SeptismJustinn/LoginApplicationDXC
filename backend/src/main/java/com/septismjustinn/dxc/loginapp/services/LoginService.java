074e1943927e511933919a13c750fcdbc267554e
Tan Justinn <ohhoehoe@gmail.com>
2023-10-04 16:44:31 +0800

    Added Transactional annotation
    -To mark function as transactional and identify it as trying to modify the db?

--- backend/src/main/java/com/septismjustinn/dxc/loginapp/services/LoginService.java
+++ backend/src/main/java/com/septismjustinn/dxc/loginapp/services/LoginService.java
@@ -4,6 +4,7 @@ import com.septismjustinn.dxc.loginapp.data.LoginRepository;
 import com.septismjustinn.dxc.loginapp.data.UserRepository;
 import com.septismjustinn.dxc.loginapp.models.Login;
 import com.septismjustinn.dxc.loginapp.models.User;
+import jakarta.transaction.Transactional;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -30,14 +31,17 @@ public class LoginService {
         return user;
     }
 
+    @Transactional
     public boolean registerLogin(UUID jti, User user) {
         try {
             // Remove all previous user logins
             loginRepo.deleteByUser(user);
+            loginRepo.flush();
             loginRepo.save(new Login(jti, user));
             return true;
         } catch (Exception e) {
             // In case of errors in database
+            System.out.println(e.getMessage());
             return false;
         }
     }
