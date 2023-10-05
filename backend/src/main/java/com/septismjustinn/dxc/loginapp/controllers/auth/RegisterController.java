56ce07701331d00ab7b2616ec39fde6d886fcdb9
Tan Justinn <ohhoehoe@gmail.com>
2023-10-05 14:49:31 +0800

    Validated register requests
    -Also added documentation

--- backend/src/main/java/com/septismjustinn/dxc/loginapp/controllers/auth/RegisterController.java
+++ backend/src/main/java/com/septismjustinn/dxc/loginapp/controllers/auth/RegisterController.java
@@ -2,6 +2,8 @@ package com.septismjustinn.dxc.loginapp.controllers.auth;
 
 import com.septismjustinn.dxc.loginapp.models.User;
 import com.septismjustinn.dxc.loginapp.services.UserService;
+import com.septismjustinn.dxc.loginapp.validators.RegisterRequest;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,11 +23,16 @@ public class RegisterController {
         this.userService = userService;
     }
 
+    /**
+     * Endpoint to register user
+     * @param req Request body containing valid name, username and password
+     * @return Response {status: true, content: created user object} or {status: false, message: error message}
+     */
     @PostMapping
-    public ResponseEntity<Map<String, Object>> addNewUser(@RequestBody Map<String, String> req) {
+    public ResponseEntity<Map<String, Object>> addNewUser(@RequestBody @Valid RegisterRequest req) {
         Map<String, Object> res = new HashMap<>();
         try {
-            User newUser = userService.createUser(req.get("name"), req.get("username"), req.get("password"));
+            User newUser = userService.createUser(req.getName(), req.getUsername(), req.getPassword());
             res.put("content", newUser);
             res.put("status", true);
             return new ResponseEntity(res, HttpStatus.OK);
