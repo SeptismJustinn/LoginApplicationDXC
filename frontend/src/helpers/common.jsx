d82e83e3788769859eb57eaddd60d76347c4b0ba
Tan Justinn <ohhoehoe@gmail.com>
2023-10-05 15:10:38 +0800

    Added helper function for fetching data from API

---
+++ frontend/src/helpers/common.jsx
@@ -0,0 +1,29 @@
+export async function fetchData(endpoint, token, method, body) {
+  const res = await fetch(import.meta.env.VITE_SERVER + endpoint, {
+    method,
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + token,
+    },
+    body: JSON.stringify(body),
+  });
+  const data = await res.json();
+
+  let returnValue = {};
+
+  if (res.ok) {
+    if (!data.status) {
+      returnValue = { ok: false, data: data.message };
+    } else {
+      returnValue = { ok: true, data: data.content };
+    }
+  } else {
+    if (data?.status) {
+      returnValue = { ok: false, data: data.message };
+    } else {
+      console.log(data);
+      returnValue = { ok: false, data: "An error has occurred" };
+    }
+  }
+  return returnValue;
+}
