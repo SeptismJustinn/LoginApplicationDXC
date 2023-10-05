009fb8c355c8eb7926401761a3c7f1cb3b20dc1e
Tan Justinn <ohhoehoe@gmail.com>
2023-10-06 00:17:38 +0800

    Added showRegister state to conditionally render register form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,22 +4,25 @@ import User from "./helpers/objectClasses";
 import UserProfile from "./components/UserProfile";
 import UsersListToggle from "./components/UsersListToggle";
 import styles from "./css/App.module.css";
+import UsersList from "./components/UsersList";
+import Register from "./components/Register";
 
 function App() {
   const [user, setUser] = useState(new User());
   const [showUsers, setShowUsers] = useState(false);
+  const [showRegister, setShowRegister] = useState(false);
   if (user.role == "ADMIN") {
     if (showUsers) {
       return (
         <div className={styles.main_page}>
-          <UsersListToggle setShowUsers={setShowUsers} />
-          <UsersList />
+          <UsersListToggle showUsers={showUsers} setShowUsers={setShowUsers} />
+          <UsersList user={user} />
         </div>
       );
     } else {
       return (
         <div className={styles.main_page}>
-          <UsersListToggle setShowUsers={setShowUsers} />
+          <UsersListToggle showUsers={showUsers} setShowUsers={setShowUsers} />
           <UserProfile user={user} />
         </div>
       );
@@ -31,11 +34,19 @@ function App() {
       </div>
     );
   } else {
-    return (
-      <div className={styles.main_page}>
-        <Login setUser={setUser} />
-      </div>
-    );
+    if (showRegister) {
+      return (
+        <div className={styles.main_page}>
+          <Register setShowRegister={setShowRegister} />
+        </div>
+      );
+    } else {
+      return (
+        <div className={styles.main_page}>
+          <Login setUser={setUser} setShowRegister={setShowRegister} />
+        </div>
+      );
+    }
   }
 }
 
