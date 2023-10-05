d47681ac770b32455bb094e0190879d7cc345ec9
Tan Justinn <ohhoehoe@gmail.com>
2023-10-06 03:03:32 +0800

    Added @JsonCreator annotation
    -Important for single-attribute requests

--- backend/src/main/java/com/septismjustinn/dxc/loginapp/validators/DecodeRequest.java
+++ backend/src/main/java/com/septismjustinn/dxc/loginapp/validators/DecodeRequest.java
@@ -1,5 +1,6 @@
 package com.septismjustinn.dxc.loginapp.validators;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import jakarta.validation.constraints.NotNull;
 import org.hibernate.validator.constraints.Length;
 
@@ -8,6 +9,8 @@ public class DecodeRequest {
     @Length(min=1)
     private String plainText;
 
+    // This annotation is important for deserializing single-attribute requests for some reason.
+    @JsonCreator
     public DecodeRequest(@NotNull String plainText) {
         this.plainText = plainText;
     }
