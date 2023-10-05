639d96a9a7d9a9a45555a7c38f8f3b75d80375f1
Tan Justinn <ohhoehoe@gmail.com>
2023-10-05 16:39:14 +0800

    Setup UserProfile component

--- frontend/src/components/UserProfile.jsx
+++ frontend/src/components/UserProfile.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
-function UserProfile() {
-  return <div>UserProfile</div>;
+function UserProfile(props) {
+  return (
+    <div>
+      <div>
+        <div>Name: </div>
+        <div>{props.user.name}</div>
+      </div>
+      <div>
+        <div>Username: </div>
+        <div>{props.user.username}</div>
+      </div>
+      <div>
+        <div>Role: </div>
+        <div>{props.user.role}</div>
+      </div>
+    </div>
+  );
 }
 
 export default UserProfile;
