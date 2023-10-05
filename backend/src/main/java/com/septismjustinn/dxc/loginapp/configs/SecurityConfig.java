f715d15bd30c2a57c3eb2fecb3445c6a71bcd354
Tan Justinn <ohhoehoe@gmail.com>
2023-10-05 16:36:25 +0800

    Disabled Spring Security 6 cors
    -Too poorly documented to use properly

--- backend/src/main/java/com/septismjustinn/dxc/loginapp/configs/SecurityConfig.java
+++ backend/src/main/java/com/septismjustinn/dxc/loginapp/configs/SecurityConfig.java
@@ -1,14 +1,12 @@
 package com.septismjustinn.dxc.loginapp.configs;
 
 import com.septismjustinn.dxc.loginapp.data.UserRepository;
-import com.septismjustinn.dxc.loginapp.models.Role;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -41,6 +39,7 @@ public class SecurityConfig {
                 .authenticationProvider(authenticationProvider()).addFilterBefore(
                         jwtFilter, UsernamePasswordAuthenticationFilter.class
                 )
+                .cors((cors) -> cors.disable())
                 .csrf((csrf) -> csrf.disable())
                 .build();
         // Disable CSRF since authentication will be done via Tokens for protected endpoints
