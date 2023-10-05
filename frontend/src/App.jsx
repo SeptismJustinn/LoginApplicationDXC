4b867dccf32475b8ab5b241ecaeff1993215f944
Tan Justinn <ohhoehoe@gmail.com>
2023-10-05 15:09:33 +0800

    Added conditons to render page based on permissions

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,41 @@
 import React from "react";
+import Login from "./components/Login";
+import User from "./helpers/objectClasses";
+import UserProfile from "./components/UserProfile";
+import UsersListToggle from "./components/UsersListToggle";
 
 function App() {
-  return (
-    <div>
-      <h2>GA SEI</h2>
-    </div>
-  );
+  const [user, setUser] = useState(new User());
+  const [showUsers, setShowUsers] = useState(false);
+  if (user.role == "ADMIN") {
+    if (showUsers) {
+      return (
+        <div>
+          <UsersListToggle setShowUsers={setShowUsers} />
+          <UsersList />
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <UsersListToggle setShowUsers={setShowUsers} />
+          <UserProfile user={user} />
+        </div>
+      );
+    }
+  } else if (user.role == "USER") {
+    return (
+      <div>
+        <UserProfile user={user} />
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <Login setUser={setUser} />
+      </div>
+    );
+  }
 }
 
 export default App;
